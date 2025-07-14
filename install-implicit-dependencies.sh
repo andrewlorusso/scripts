3695abd01a109c94fcf7f57b6f0b16d2ecfbe797
andrewlorusso <194324572+andrewlorusso@users.noreply.github.com>
2025-07-14 00:00:49 -0400

    Update install-implicit-dependencies.sh

--- install-implicit-dependencies.sh
+++ install-implicit-dependencies.sh
@@ -1,1 +1,22 @@
-# Use git submodules to treat your scripts repo as a separate, linked project inside your main dotfiles repo, keeping their histories independent but allowing you to clone everything at once with the git clone --recurse-submodules command.
+#!/usr/bin/env zsh
+# purpose: installs additional dependencies the user defines in their configuration files.
+# example: the font package used by the terminal emulator is not tracked by the system package manager.
+# usage: use with dotfiles repo
+
+# ~/.dotfiles/package/.ideps.yml
+# ideps:
+#   ripgrep:
+#     desc: "search for telescope"
+#     managers: { any: ripgrep }
+#
+#   clangd:
+#     desc: "C++ LSP"
+#     managers:
+#       brew: llvm
+#       apt: clangd
+#       pacman: clang
+
+pkg_managers=(brew apt)
+for mg in "${(@)pkg_managers}"; do
+done
+pkg_manager=
